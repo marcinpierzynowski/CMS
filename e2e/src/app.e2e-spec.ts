c13d92c9cf0a1825c66a6a867c329efda72357f3
marcinpierzynowski <marcin111974@wp.pl>
2019-06-30 22:14:41 +0200

    Update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to muszelka-shop-admin!');
+    expect(page.getTitleText()).toEqual('Welcome to shop-admin!');
   });
 });
