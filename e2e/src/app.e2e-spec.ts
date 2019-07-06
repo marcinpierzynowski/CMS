e56b081c53c02b5c70dcdddc9e2c77c1bcf31b17
marcinpierzynowski <marcin111974@wp.pl>
2019-07-06 17:07:54 +0200

    Update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to stoma-cms-admin!');
+    expect(page.getTitleText()).toEqual('Welcome to stoma-shop-admin!');
   });
 
   afterEach(async () => {
